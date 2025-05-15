ce7699abdef0d9a74904e59218d4e97393e6c523
Tiagoguimaraesunivesp <tiagonaunivesp@gmail.com>
2025-05-15 16:16:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-document.getElementById('formAgendamento').addEventListener('submit', function (e) {
-  e.preventDefault();
-
-  const nome = document.getElementById('nome').value;
-  const data = document.getElementById('data').value;
-  const hora = document.getElementById('hora').value;
-
-  const lista = document.getElementById('listaAgendamentos');
-  const item = document.createElement('li');
-  item.textContent = `${nome} - ${data} às ${hora}`;
-  lista.appendChild(item);
-
-  // Limpa o formulário
-  document.getElementById('formAgendamento').reset();
-});
-
+<img src="img/foto.equipe.jpeg" alt="Equipe L' Brothers">
